12748d9de5bf5a92c39a4dc3ced208d8cbbcc057
Richard He <richard@heyucheng.com>
2016-10-15 09:23:43 -0500

    Revert "Revert "Revert "added error fetching groloc condition"""
    This reverts commit 8ae725e4feb6c69a6fd0bd1dd2bd6889fcd12620.

--- client/reactive-maps-example.js
+++ client/reactive-maps-example.js
@@ -1,6 +1,4 @@
 if (Meteor.isClient) {
-    var MAP_ZOOM = 15;
-
   Template.map.onCreated(function() {
     GoogleMaps.ready('map', function(map) {
       google.maps.event.addListener(map.instance, 'click', function(event) {
@@ -50,20 +48,16 @@ if (Meteor.isClient) {
     GoogleMaps.load();
   });
 
-    Template.map.helpers({
-        geolocationError: function() {
-            var error = Geolocation.error();
-            return error && error.message;
-        },
-        mapOptions: function() {
-            var latLng = Geolocation.latLng();
-            // Initialize the map once we have the latLng.
-            if (GoogleMaps.loaded() && latLng) {
-                return {
-                    center: new google.maps.LatLng(latLng.lat, latLng.lng),
-                    zoom: MAP_ZOOM
-                };
-            }
+  Template.map.helpers({
+    mapOptions: function() {
+        var latLng = Geolocation.latLng();
+        // Initialize the map once we have the latLng.
+        if (GoogleMaps.loaded()) {
+            return {
+                center: new google.maps.LatLng(latLng.lat, latLng.lng),
+                zoom: 15
+            };
         }
-    });
+    }
+  });
 }
